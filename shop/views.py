e038c2c882a4f9bd6514c3c0fcd50b159da1fb6a
sungatishe <210103144@stu.sdu.edu.kz>
2024-05-27 21:53:25 +0500

    Added new page, pagination, some functionality.

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
+
 
 def index(request):
     product_objects = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(item_name__icontains=item_name)
+
+    paginator = Paginator(product_objects, 4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
     return render(request, 'shop/index.html', {'product_objects': product_objects})
+
+
+def details(request, id):
+    product_object = Product.objects.get(id=id)
+    return render(request, 'shop/detail.html', {'product_object': product_object})
